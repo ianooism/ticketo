63b03d8f7c141683ac8662d404835a6df2cf581f
Ian Ooi <ianooism@gmail.com>
2016-12-23 06:22:36 +0000

    initialize state

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :ticket
   belongs_to :owner, class_name: 'User'
+  belongs_to :state
   
   validates :body, presence: true
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,6 +3,7 @@ class Ticket < ApplicationRecord
   
   belongs_to :project
   belongs_to :owner, class_name: 'User'
+  belongs_to :state
   
   has_many :comments
   
