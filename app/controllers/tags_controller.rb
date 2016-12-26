410648b1dda376291d8854429bcb524bec3c6c75
Ian Ooi <ianooism@gmail.com>
2016-12-26 00:52:50 +0000

    add link to destroy tag on ticket

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,13 +1,20 @@
 class TagsController < ApplicationController
   def destroy
+    current_tag.destroy
+    redirect_to project_ticket_url(current_project, current_ticket),
+      notice: "Tag destroyed."
   end
   
   private
-    def ticket
-      Ticket.find(params[:ticket_id])
+    def current_ticket
+      @ticket ||= Ticket.find(params[:ticket_id])
     end
     
-    def tag
-      ticket.tags.find(params[:id])
+    def current_tag
+      @tag ||= current_ticket.tags.find(params[:id])
+    end
+    
+    def current_project
+      @project ||= current_ticket.project
     end
 end
